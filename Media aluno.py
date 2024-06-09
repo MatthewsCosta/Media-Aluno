44bd214aa28fe4454693a2174c39530ff19adbec
Matheus Costa <matheusxcosta@gmail.com>
2024-06-09 12:10:01 -0300

    atualização
    organização/ajustes

--- Media aluno.py
+++ Media aluno.py
@@ -11,11 +11,14 @@ media2 = (an3 + an4) / 2
 print(f"A média de {aluno1} foi {media1}, e a média de {aluno2} foi {media2}.")
 
 print("o historico de notas dos alunos ficou:")
-print(f"{aluno1}  e  {aluno2}")
-print(f"{an1}  e  {an3} na primeira avaliação")
-print(f"{an2}   e  {an4} na segunda avaliação")
-print(f"{media1}  e  {media2} na média")
-if media1 > media2:
+print(f"|{aluno1} || {aluno2} |")
+print(f"  {an1}  e  {an3} NOTA-1")
+print(f"  {an2}  e  {an4} NOTA-2")
+print(f"  {media1}  e  {media2} MÉDIA FINAL")
+
+if media1 == media2:
+    print(f"{aluno1} e {aluno2} tiraram a mesma média:{media1}")
+elif media1 > media2:
     print(f"{aluno1} tirou a maior nota")
 else:
     print(f"{aluno2} tirou a maior nota")
