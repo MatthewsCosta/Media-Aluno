528e4db9f337cdff428a3211c39cf73d52a044b0
Matheus Costa <matheusxcosta@gmail.com>
2024-06-09 11:35:16 -0300

    Atualização de media aluno 1

--- Media aluno.py
+++ Media aluno.py
@@ -1,3 +1,7 @@
-aluno1 = (input("digite seu nome: ")
+aluno1 = (input("digite seu nome: "))
 an1 = int(input("Digite a primeira nota > "))
 an2 = int(input("Digite a seguranda nota > "))
+
+media1 = (an1+an2)/2
+print (f"A média de {aluno1} é {media1}")
+
