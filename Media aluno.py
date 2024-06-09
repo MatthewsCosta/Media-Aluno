3260ff1c2082f693daf88c060c4e26ec258758d8
Matheus Costa <matheusxcosta@gmail.com>
2024-06-09 11:59:37 -0300

    Atualização

--- Media aluno.py
+++ Media aluno.py
@@ -1,16 +1,21 @@
-aluno1 = input("digite seu nome: ")
-an1 = int(input("Digite a primeira nota > "))
-an2 = int(input("Digite a segunda nota > "))
+aluno1 = str(input("digite seu nome: "))
+an1 = float(input("Digite a primeira nota > "))
+an2 = float(input("Digite a segunda nota > "))
 
-aluno2 = input("digite seu nome: ")
-an3 = int(input("Digite a primeira nota > "))
-an4 = int(input("Digite a segunda nota > "))
+aluno2 = str(input("digite seu nome: "))
+an3 = float(input("Digite a primeira nota > "))
+an4 = float(input("Digite a segunda nota > "))
 
-media1 = (an1+an2)/2
-media2 = (an3+an4)/2
+media1 = (an1 + an2) / 2
+media2 = (an3 + an4) / 2
 print(f"A média de {aluno1} foi {media1}, e a média de {aluno2} foi {media2}.")
 
+print("o historico de notas dos alunos ficou:")
+print(f"{aluno1}  e  {aluno2}")
+print(f"{an1}  e  {an3} na primeira avaliação")
+print(f"{an2}   e  {an4} na segunda avaliação")
+print(f"{media1}  e  {media2} na média")
 if media1 > media2:
-    print (f"{aluno1} tirou a maior nota")
+    print(f"{aluno1} tirou a maior nota")
 else:
-    print (f"{aluno2} tirou a maior nota")
+    print(f"{aluno2} tirou a maior nota")
